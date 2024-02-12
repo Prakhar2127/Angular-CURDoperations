b998848a7ac93d6aa5a1791372fb0b1ff5d666de
Prakhar Bansal <95363759+Prakhar2127@users.noreply.github.com>
2024-02-12 12:27:54 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,51 +5,69 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { ShopAddEditComponent } from './shop-add-edit/shop-add-edit.component';
-import {MatTableModule, MatTableDataSource} from '@angular/material/table';
+import {MatTableModule,} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { DatePipe } from '@angular/common';
 
-export interface ShopTable {
-  sNo: number;
-  shopName: string;
-  shopId: number;
-  shopDetails: string;
-  contactNo: number;
-  estd: string;
-  country: string;
-  action: string
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatDialogModule, MatTableModule, MatInputModule, MatFormFieldModule, ShopAddEditComponent],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatDialogModule, MatTableModule, MatInputModule, 
+            MatFormFieldModule, ShopAddEditComponent, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'shop-app';
 
-  constructor(private _dialog: MatDialog) {
+  constructor(private _dialog: MatDialog, ) {
   }
 
   openAddEditShopForm() {
     this._dialog.open(ShopAddEditComponent);
   }
 
-  displayedColumns: string[] = ['sNo', 'shopName', 'shopId', 'shopDetails','contactNo','estd','country', 'action'];
-  dataSource = new MatTableDataSource<any>;
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  shopData: any;
+  //shopData: any[] = [];
+  displayedColumns: string[] = ['sno', 'shopName', 'shopId', 'shopDetails', 'contactNo', 'estd', 'country', 'action'];
+  ngOnInit() {
+    try {
+      const data = localStorage.getItem('myData');
+      if(data) {
+        this.shopData = JSON.parse(data); 
+        console.log(this.shopData);
+      } else {
+        console.error("No shop data found in local storage");
+      }
+    }catch(error) {
+      console.error("Error parsing shopdata:",error);
+    }
   }
+  // openAddEditShopForme(shop?: any) {
+  //   this._dialog.open(ShopAddEditComponent, { data: shop });
+  // }
 
-  onChildClick(array: string[]) {
-    //console.log(array);
-    this.dataSource = new MatTableDataSource(array);
-    localStorage.setItem('myData',JSON.stringify(array));
-    const shopData = JSON.parse(localStorage.getItem('myData') as string);
-    console.log(shopData);
-  }
+  // addShop(shop: any) {
+  //   this.shopData.push(shop);
+  //   this.saveToLocalStorage();
+  // }
+
+  // editShop(shop: any) {
+  //   const index = this.shopData.findIndex((s) => s.shopId === shop.shopId);
+  //   this.shopData[index] = shop;
+  //   this.saveToLocalStorage();
+  // }
+
+  // deleteShop(shop: any) {
+  //   const index = this.shopData.findIndex((s) => s.shopId === shop.shopId);
+  //   this.shopData.splice(index, 1);
+  //   this.saveToLocalStorage();
+  // }
+
+  // saveToLocalStorage() {
+  //   localStorage.setItem('myData', JSON.stringify(this.shopData));
+  // }
+  
 }
